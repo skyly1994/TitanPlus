c4ee4abd7a4c6bca7b147854004f0c0ae3587828
陆斌 <2418840172@qq.com>
2016-09-13 12:30:59 +0800

    Initial Commit

---
+++ TitanPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TitanPlus
+//
+//  Created by 陆斌 on 16/9/13.
+//  Copyright © 2016年 陆斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
